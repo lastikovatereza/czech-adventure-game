b024505a60f53a5bcfccceb4d90f38971d744e4b
lastikovatereza <lastikova.tereza@gmail.com>
2025-09-05 23:10:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ from Cviceni.hra import Hra
 hra = Hra()
 prikaz = ""
 
-# Zde dokonči úlohu svým kódem...
-
 hra.vypis_lokaci()
 
 while True:
     prikaz = input("\nZadej příkaz: ")
     if not hra.zpracuj_prikaz(prikaz):
+
         break
