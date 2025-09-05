78f0bffd6d229d3bc8f907741ff9a5f32b8046b9
lastikovatereza <lastikova.tereza@gmail.com>
2025-09-05 23:07:27 +0200

    Přidána třída Lokace pro popis herních lokací
    Atributy: název, popis, sever, jih, východ, západ; metoda dostupne_smery() pro vypsání možných směrů.

---
+++ lokace.py
@@ -0,0 +1,24 @@
+class Lokace:
+    # Lokace na severu
+    sever = None
+
+    # Lokace na jihu
+    jih = None
+
+    # Zde dokonči úlohu svým kódem...
+
+    def __init__(self, nazev, popis):
+        self.nazev = nazev
+        self.popis = popis
+        self.sever = None
+        self.jih = None
+        self.vychod = None
+        self.zapad = None
+
+    def dostupne_smery(self):
+        smery = []
+        if self.sever: smery.append("sever")
+        if self.jih: smery.append("jih")
+        if self.vychod: smery.append("východ")
+        if self.zapad: smery.append("západ")
+        return smery
